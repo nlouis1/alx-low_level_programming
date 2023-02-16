11bf3c76005138f8aa64c755d0fe3e6817cecd50
nlouis1 <louis.ntwali@gmail.com>
2023-02-16 12:26:34 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*function main
+ * is now going to plint a message
+ * and return 0*/
 int main(void)
 {
+	/*displaying a message using putts method*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
