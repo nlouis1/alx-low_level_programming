38298206ee4108438de7e2d60d6d57a4dcb10273
nlouis1 <louis.ntwali@gmail.com>
2023-02-17 15:58:20 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -A program to dispalay alphabet
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while
+		(c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
