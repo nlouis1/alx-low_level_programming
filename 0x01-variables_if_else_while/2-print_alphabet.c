3feb85e098da6285e27d525bd235920ee09a0940
nlouis1 <louis.ntwali@gmail.com>
2023-02-17 14:55:14 -0800

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -A program to uppercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
