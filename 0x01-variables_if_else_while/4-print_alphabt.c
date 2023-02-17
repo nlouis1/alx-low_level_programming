496c36feb626caf3f48d4234656c56e589058715
nlouis1 <louis.ntwali@gmail.com>
2023-02-17 15:20:40 -0800

    When I was having that alphabet sou

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -A program starting
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while
+		(c <= 'z')
+		{
+			if (c != 'q' && c != 'e')
+			{
+				putchar(c);
+			}
+			c++;
+		}
+	putchat('\n');
+	return (0);
+}
