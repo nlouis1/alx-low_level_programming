6d51ded32e3022eb0bc728a0b78c4546df3cfd51
nlouis1 <louis.ntwali@gmail.com>
2023-02-17 16:30:31 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -A program for 9th
+ * Return: 0 (success);
+ */
+int main(void)
+	{
+		int a;
+
+		for (a = 0; a < 10; a++)
+		{
+			putchar(a + '0');
+			if (a < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+	}
