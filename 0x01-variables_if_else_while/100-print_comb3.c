2475f777d24c8fb801f12da64112a41460678b31
nlouis1 <louis.ntwali@gmail.com>
2023-02-17 23:50:45 -0800

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -A program number 1 in special
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 90; a++)
+	{
+		if (a < 10)
+		{
+			putchar('0' + a);
+		}
+		else
+		{
+			putchar(a + '0');
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
