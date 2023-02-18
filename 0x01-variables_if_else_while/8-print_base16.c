b459cd50c1fd97ac9db7a50438c1e7ee26b5dd30
nlouis1 <louis.ntwali@gmail.com>
2023-02-17 16:08:14 -0800

    Patience, persistence and perspiration mak

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -A program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+	int d = 0;
+
+	while
+		(d < 10)
+		{
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= f)
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
