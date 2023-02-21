367bc2910b5ad63708ac2cde42b4ffc3ae3f2a22
nlouis1 <louis.ntwali@gmail.com>
2023-02-21 06:03:13 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
 {
-int i, j;
+	int c, i, k, j;
 
-for (i = 0; i < 100; i++)
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
 
-{
-for (j = 0; j < 100; j++)
-{
-if (i < j)
-{
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
-putchar(' ');
-putchar((j / 10) + 48);
-putchar((j % 10) + 48);
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-
-}
-
-putchar('\n');
-
-return (0);
-
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			} n 
+		}
+	}
+	putchar('\n');
+	return (0);
 }
