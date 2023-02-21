fb7f0950095d9210554ae22c9f1ccf332d78b157
nlouis1 <louis.ntwali@gmail.com>
2023-02-21 06:51:18 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -printing a character
+ * followed by a new line
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	putchar('_');
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
