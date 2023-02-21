46d1b05fe52ff0017b028724be8cfbd25e324e21
nlouis1 <louis.ntwali@gmail.com>
2023-02-21 05:59:18 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
-/**
- * main -A progra last task
- * Return: 0(success)
- */
-int main(void)
-{
-	int a, b, j, i;
 
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9 ; b++)
-		{
-			for (i = a; i <= 9; i++)
-			{
-				for (j = 0; j <= 9; j++)
-				{
-					if (!(a == i && b <= j))
-					{
-						putchar('0' + a);
-						putchar('0' + b);
-						putchar(' ');
-						putchar('0' + i);
-						putchar('0' + j);
-						if (!(a == 9 && b == 8 && i == 9 && j == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int i, j;
+
+for (i = 0; i < 100; i++)
+
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+}
+
+putchar('\n');
+
+return (0);
+
 }
