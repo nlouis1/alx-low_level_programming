fe0429e70ab16c711526e428eaaaccc0f5d56781
nlouis1 <louis.ntwali@gmail.com>
2023-02-22 00:27:58 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main -Print apha 10x
+ * Return :0 (always Success)
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -display alphabet 10X
+ * Return:void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c < 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('\n');
+		}
+		i++;
+	}
+}
