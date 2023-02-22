c180a2783eeb67cfcdbc1d2de2a9d940012e12a4
nlouis1 <louis.ntwali@gmail.com>
2023-02-22 01:13:48 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower -check for lowercase
+ * Return: 1 for lowercase or 0 for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 29 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
