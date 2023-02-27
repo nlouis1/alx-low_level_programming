c49587a83c29c8aa6ac01800121aa1ad43f82699
nlouis1 <louis.ntwali@gmail.com>
2023-02-27 14:30:52 -0800

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *swap_int -swap two numbers numbers
+ * Return nothing
+ * address of a,b;
+ * @a: address to store data
+ * @b: stored to store a second number
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*b = *a;
+	*a = m;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -18,5 +18,6 @@ void times_table(void);
 int add(int,int);
 void print_to_98(int n);
 void print_times_table(int n);
+void swap_int(int *a, int *b);
 
 #endif
