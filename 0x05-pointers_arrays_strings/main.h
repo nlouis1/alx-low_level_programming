327c3941e31c0ca976ce2e4b0af2d7a471dbd5f8
nlouis1 <louis.ntwali@gmail.com>
2023-02-27 15:52:53 -0800

    Winning is only half of it

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void reset_to_98(int *n);
 void _putchar(char c);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 void print_rev(char *s);
 void _puts(char *str);
 void print_alphabet(void);
