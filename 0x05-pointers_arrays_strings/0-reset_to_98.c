642757526a558ff45ac4c447155efbe6a22dd567
nlouis1 <louis.ntwali@gmail.com>
2023-02-27 14:17:04 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 /**
  * reset_to_98 -method to
  * change a valueof a point
- * @n to change the original file
- * void Return type
+ * @n: to change the original file
+ * void Return type(success)
  */
 void reset_to_98(int *n)
 {
